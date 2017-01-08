54f24017610aa79a62cdf0ff622f14227d7640a3
hotshotGG <100332338@qq.com>
2017-01-08 14:23:44 +0800

    update

--- app/src/main/java/io/netopen/hotbitmapgg/rxjava2demo/MainActivity.java
+++ app/src/main/java/io/netopen/hotbitmapgg/rxjava2demo/MainActivity.java
@@ -4,10 +4,13 @@ import io.reactivex.BackpressureStrategy;
 import io.reactivex.Flowable;
 import io.reactivex.FlowableEmitter;
 import io.reactivex.FlowableOnSubscribe;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
+import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
 
@@ -40,6 +43,28 @@ public class MainActivity extends AppCompatActivity {
 
 
           @Override public void onComplete() {
+
+          }
+        });
+
+    Flowable.just("rxjava2demo")
+        .map(new Function<String, String>() {
+
+          @Override public String apply(String s) throws Exception {
+            return s;
+          }
+        })
+        .flatMap(new Function<String, Publisher<String>>() {
+          @Override public Publisher<String> apply(String s) throws Exception {
+            return Flowable.just("sssss");
+          }
+        })
+        .subscribe(new Consumer<String>() {
+          @Override public void accept(String s) throws Exception {
+            Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
+          }
+        }, new Consumer<Throwable>() {
+          @Override public void accept(Throwable throwable) throws Exception {
 
           }
         });
