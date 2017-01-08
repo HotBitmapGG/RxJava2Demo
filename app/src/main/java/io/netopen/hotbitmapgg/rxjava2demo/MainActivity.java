5ce95cd9f4c8f5bd2fa3e242c300efa43ac98058
hotshotGG <100332338@qq.com>
2017-01-08 14:14:35 +0800

    update

--- app/src/main/java/io/netopen/hotbitmapgg/rxjava2demo/MainActivity.java
+++ app/src/main/java/io/netopen/hotbitmapgg/rxjava2demo/MainActivity.java
@@ -1,7 +1,15 @@
 package io.netopen.hotbitmapgg.rxjava2demo;
 
-import android.support.v7.app.AppCompatActivity;
+import io.reactivex.BackpressureStrategy;
+import io.reactivex.Flowable;
+import io.reactivex.FlowableEmitter;
+import io.reactivex.FlowableOnSubscribe;
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,31 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    Flowable.create(new FlowableOnSubscribe<String>() {
+      @Override public void subscribe(FlowableEmitter<String> e) throws Exception {
+        e.onNext("rxjava2");
+      }
+    }, BackpressureStrategy.BUFFER)
+        .subscribe(new Subscriber<String>() {
+          @Override public void onSubscribe(Subscription s) {
+            s.request(Long.MAX_VALUE);
+          }
+
+
+          @Override public void onNext(String s) {
+            Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
+          }
+
+
+          @Override public void onError(Throwable t) {
+
+          }
+
+
+          @Override public void onComplete() {
+
+          }
+        });
   }
 }
