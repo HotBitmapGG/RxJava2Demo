b3981b0ed8b259236c8876815099ccdb865dc6b0
hotshotGG <100332338@qq.com>
2017-01-12 13:43:11 +0800

    Add JackOptions and Java8

---
+++ app/src/main/java/io/netopen/hotbitmapgg/rxjava2demo/Java8DemoActivity.java
@@ -0,0 +1,22 @@
+package io.netopen.hotbitmapgg.rxjava2demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Java8DemoActivity extends AppCompatActivity {
+
+  private List<String> strs = new ArrayList<>();
+
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_java8_demo);
+
+
+
+  }
+}
