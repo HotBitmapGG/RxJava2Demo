fc4250dd75335812ac77d812fb88f35fac12eb9a
hotshotGG <100332338@qq.com>
2017-03-01 18:52:33 +0800

    Update

---
+++ app/src/main/java/io/netopen/hotbitmapgg/rxjava2demo/drawable/CustomDrawable.java
@@ -0,0 +1,34 @@
+package io.netopen.hotbitmapgg.rxjava2demo.drawable;
+
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by hcc on 2017/3/1 18:25
+ * 100332338@qq.com
+ * RxJava2Demo
+ *
+ * @HotBitmapGG
+ */
+
+public class CustomDrawable extends Drawable {
+  @Override public void draw(Canvas canvas) {
+
+  }
+
+
+  @Override public void setAlpha(int i) {
+
+  }
+
+
+  @Override public void setColorFilter(ColorFilter colorFilter) {
+
+  }
+
+
+  @Override public int getOpacity() {
+    return 0;
+  }
+}
