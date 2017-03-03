00e1c6e5173a934c2c99c8473d8946a0fdabece5
hotshotGG <100332338@qq.com>
2017-03-03 08:49:23 +0800

    Add

---
+++ app/src/main/java/io/netopen/hotbitmapgg/rxjava2demo/test/HomeBean.java
@@ -0,0 +1,739 @@
+package io.netopen.hotbitmapgg.rxjava2demo.test;
+
+import java.util.List;
+
+/**
+ * Created by hcc on 2017/3/2 09:20
+ * 100332338@qq.com
+ * RxJava2Demo
+ *
+ * @HotBitmapGG
+ */
+
+public class HomeBean {
+
+  /**
+   * Data : {"系统公告":[{"类型":"系统公告","发布时间":"2017-02-20T06:00:30","有效期":0,"优先级":1,"标题":"李克强打叉的\u201c万里审批图\u201d成历史","内容":"https://www.home10000.net/799个审批工作日缩短为30个：李克强打叉的\u201c万里审批图\u201d成历史_滚动新闻_中国政府网.htm","id":5}],"广告轮播":[{"类型":"广告轮播","发布时间":"2017-02-28T08:40:43","有效期":0,"优先级":1,"标题":"http://yssoft-oss.oss-cn-shenzhen.aliyuncs.com/Images/013f6471bb9148bd8b663856ecf1269c.jpg?Expires=1488329393&OSSAccessKeyId=LTAIyA0qfsmsrDLZ&Signature=IwGfiVbGfeCg9qVke1U%2Fp3ur6dk%3D","内容":"https://www.home10000.net/文章经纪人是谁？叫什么名字？文章经纪人回应文章出轨_深窗网.htm","id":6},{"类型":"广告轮播","发布时间":"2017-02-28T08:40:43","有效期":0,"优先级":1,"标题":"http://yssoft-oss.oss-cn-shenzhen.aliyuncs.com/Images/090843de4890456da4a7193272a59052.jpg?Expires=1488329393&OSSAccessKeyId=LTAIyA0qfsmsrDLZ&Signature=YHRpcNjxwhNBEKywf%2FGv6qDiD8g%3D","内容":"https://www.home10000.net/关于房产经纪人的励志文章.htm","id":7}],"城市列表":[{"区域名称":"武汉市","id_qy_parent":0,"经度":114.299,"纬度":30.5844,"级别":1,"显示顺序":19,"id":420100}],"推荐房源":[{"id_yh":311,"id_yh_wt":509,"id_yh_ys":809,"id_yh_dj":41,"id_yh_zdwt":585,"id_bm":13,"id_qy":420105005,"id_lp":312,"房源编号":"GAIWOCXGVX","房源状态":"已租","盘":"店面可见","委托":1,"委托日期":"2005-07-07T21:28:32","限时":1,"有匙":1,"推荐":1,"独家":1,"装修":"简单装修","产权性质":"原始产权","产权年限":189,"出售":0,"出租":0,"租赁委托":1,"售价":9454160,"租价":7977340,"有效联系":1,"带看次数":26755,"现状":[],"房源相关文件":["http://yssoft-oss.oss-cn-shenzhen.aliyuncs.com/Images/t11.jpg?Expires=1488329397&OSSAccessKeyId=LTAIyA0qfsmsrDLZ&Signature=sZOJGZE4u923gxCOFugI0bbcr%2BM%3D"],"业主姓名":"欧吉昂","业主性别":"男","业主电话":"13257694128","业主备用电话":"15547936856","两证情况":"2年以内","电梯":1,"特色标签":[],"房源备注":"EKSLGYZBNRDHXXPIUCISVXNZCDVIRJIROFIKMXAFVVMMEOROUPCZHCEWYXTQLXCSNYTLCEAIWXUVGPEXBUCIUGT","房源审核状态":"已审核","房源待审核操作":[],"房源全员最后跟进时间":"2017-02-25T00:58:51","房源委托人最后跟进时间":"2017-02-27T01:02:47","创建时间":"2005-07-07T21:28:32","更新时间":"2017-02-27T17:59:01","座栋号":"I","单元号":2,"楼型":"","房号":2119,"总楼层":227,"楼层":217,"楼层区间":"中层","室数":4,"厅数":2,"阳台":0,"卫数":5,"厨房":2,"朝向":"阳光","建筑面积":8387950,"房屋用途":"厂房","建筑类型":"钢结构","年代":2005,"id":3},{"id_yh":212,"id_yh_wt":241,"id_yh_ys":246,"id_yh_dj":158,"id_yh_zdwt":707,"id_bm":18,"id_qy":420117004,"id_lp":12,"房源编号":"TUNYV","房源状态":"全部","盘":"区域可见","委托":0,"委托日期":"2013-07-19T21:03:44","限时":0,"有匙":0,"推荐":1,"独家":0,"装修":"精装修","产权性质":"法人产权","产权年限":325,"出售":0,"出租":0,"租赁委托":0,"售价":3586870,"租价":3494080,"有效联系":0,"带看次数":16282,"现状":[],"房源相关文件":["http://yssoft-oss.oss-cn-shenzhen.aliyuncs.com/Images/t12.jpg?Expires=1488329397&OSSAccessKeyId=LTAIyA0qfsmsrDLZ&Signature=ImABM%2F2N%2FLAvN6G4X%2BSS3Cc0NDM%3D"],"业主姓名":"欧军柱","业主性别":"女","业主电话":"13257694128","业主备用电话":"15547936857","两证情况":"无","电梯":0,"特色标签":[],"房源备注":"SHEXAEFESKIZUHSTRWCPXKQGQCTJX","房源审核状态":"已审核","房源待审核操作":[],"房源全员最后跟进时间":"2017-02-25T00:58:51","房源委托人最后跟进时间":"2017-02-27T01:02:47","创建时间":"2013-07-19T21:03:44","更新时间":"2017-02-27T17:59:01","座栋号":"13","单元号":3,"楼型":"","房号":1500,"总楼层":495,"楼层":485,"楼层区间":"低层","室数":8,"厅数":2,"阳台":4,"卫数":2,"厨房":3,"朝向":"北","建筑面积":2616140,"房屋用途":"住房","建筑类型":"混砖结构","年代":2017,"id":4},{"id_yh":78,"id_yh_wt":95,"id_yh_ys":166,"id_yh_dj":540,"id_yh_zdwt":215,"id_bm":26,"id_qy":420114001,"id_lp":88,"房源编号":"HGPJ","房源状态":"封盘","盘":"自己可见","委托":1,"委托日期":"2011-12-28T06:07:36","限时":0,"有匙":1,"推荐":1,"独家":1,"装修":"精装修","产权性质":"法人产权","产权年限":194,"出售":0,"出租":0,"租赁委托":1,"售价":4188460,"租价":7480410,"有效联系":1,"带看次数":13158,"现状":[],"房源相关文件":["http://yssoft-oss.oss-cn-shenzhen.aliyuncs.com/Images/t18.jpg?Expires=1488329397&OSSAccessKeyId=LTAIyA0qfsmsrDLZ&Signature=AQD4grDHvq5CjJ1vJkqTKEtVvYc%3D"],"业主姓名":"欧兴禄","业主性别":"女","业主电话":"13257694128","业主备用电话":"15547936863","两证情况":"2年以内","电梯":0,"特色标签":[],"房源备注":"WHYHCYTWQRQCCJPVXWPYKGOKQJTZEDSOBTKLTGMFLVZSLVWEHXYQHVSKITRNSZKWDMCUULSCENXBOKDBRNRRVSXWWWNOBXXNANQ","房源审核状态":"未审核","房源待审核操作":[],"房源全员最后跟进时间":"2017-02-25T00:58:51","房源委托人最后跟进时间":"2017-02-27T01:02:47","创建时间":"2011-12-28T06:07:36","更新时间":"2017-02-27T18:03:26","座栋号":"85","单元号":3,"楼型":"","房号":660,"总楼层":325,"楼层":315,"楼层区间":"中层","室数":9,"厅数":3,"阳台":0,"卫数":3,"厨房":4,"朝向":"阳光","建筑面积":6262840,"房屋用途":"商铺","建筑类型":"钢结构","年代":2001,"id":10}]}
+   */
+
+  private DataBean Data;
+
+
+  public DataBean getData() { return Data;}
+
+
+  public void setData(DataBean Data) { this.Data = Data;}
+
+
+  public static class DataBean {
+    private List<系统公告Bean> 系统公告;
+    private List<广告轮播Bean> 广告轮播;
+    private List<城市列表Bean> 城市列表;
+    private List<推荐房源Bean> 推荐房源;
+
+
+    public List<系统公告Bean> get系统公告() { return 系统公告;}
+
+
+    public void set系统公告(List<系统公告Bean> 系统公告) { this.系统公告 = 系统公告;}
+
+
+    public List<广告轮播Bean> get广告轮播() { return 广告轮播;}
+
+
+    public void set广告轮播(List<广告轮播Bean> 广告轮播) { this.广告轮播 = 广告轮播;}
+
+
+    public List<城市列表Bean> get城市列表() { return 城市列表;}
+
+
+    public void set城市列表(List<城市列表Bean> 城市列表) { this.城市列表 = 城市列表;}
+
+
+    public List<推荐房源Bean> get推荐房源() { return 推荐房源;}
+
+
+    public void set推荐房源(List<推荐房源Bean> 推荐房源) { this.推荐房源 = 推荐房源;}
+
+
+    public static class 系统公告Bean {
+      /**
+       * 类型 : 系统公告
+       * 发布时间 : 2017-02-20T06:00:30
+       * 有效期 : 0
+       * 优先级 : 1
+       * 标题 : 李克强打叉的“万里审批图”成历史
+       * 内容 : https://www.home10000.net/799个审批工作日缩短为30个：李克强打叉的“万里审批图”成历史_滚动新闻_中国政府网.htm
+       * id : 5
+       */
+
+      private String 类型;
+      private String 发布时间;
+      private int 有效期;
+      private int 优先级;
+      private String 标题;
+      private String 内容;
+      private int id;
+
+
+      public String get类型() { return 类型;}
+
+
+      public void set类型(String 类型) { this.类型 = 类型;}
+
+
+      public String get发布时间() { return 发布时间;}
+
+
+      public void set发布时间(String 发布时间) { this.发布时间 = 发布时间;}
+
+
+      public int get有效期() { return 有效期;}
+
+
+      public void set有效期(int 有效期) { this.有效期 = 有效期;}
+
+
+      public int get优先级() { return 优先级;}
+
+
+      public void set优先级(int 优先级) { this.优先级 = 优先级;}
+
+
+      public String get标题() { return 标题;}
+
+
+      public void set标题(String 标题) { this.标题 = 标题;}
+
+
+      public String get内容() { return 内容;}
+
+
+      public void set内容(String 内容) { this.内容 = 内容;}
+
+
+      public int getId() { return id;}
+
+
+      public void setId(int id) { this.id = id;}
+    }
+
+    public static class 广告轮播Bean {
+      /**
+       * 类型 : 广告轮播
+       * 发布时间 : 2017-02-28T08:40:43
+       * 有效期 : 0
+       * 优先级 : 1
+       * 标题 : http://yssoft-oss.oss-cn-shenzhen.aliyuncs.com/Images/013f6471bb9148bd8b663856ecf1269c.jpg?Expires=1488329393&OSSAccessKeyId=LTAIyA0qfsmsrDLZ&Signature=IwGfiVbGfeCg9qVke1U%2Fp3ur6dk%3D
+       * 内容 : https://www.home10000.net/文章经纪人是谁？叫什么名字？文章经纪人回应文章出轨_深窗网.htm
+       * id : 6
+       */
+
+      private String 类型;
+      private String 发布时间;
+      private int 有效期;
+      private int 优先级;
+      private String 标题;
+      private String 内容;
+      private int id;
+
+
+      public String get类型() { return 类型;}
+
+
+      public void set类型(String 类型) { this.类型 = 类型;}
+
+
+      public String get发布时间() { return 发布时间;}
+
+
+      public void set发布时间(String 发布时间) { this.发布时间 = 发布时间;}
+
+
+      public int get有效期() { return 有效期;}
+
+
+      public void set有效期(int 有效期) { this.有效期 = 有效期;}
+
+
+      public int get优先级() { return 优先级;}
+
+
+      public void set优先级(int 优先级) { this.优先级 = 优先级;}
+
+
+      public String get标题() { return 标题;}
+
+
+      public void set标题(String 标题) { this.标题 = 标题;}
+
+
+      public String get内容() { return 内容;}
+
+
+      public void set内容(String 内容) { this.内容 = 内容;}
+
+
+      public int getId() { return id;}
+
+
+      public void setId(int id) { this.id = id;}
+    }
+
+    public static class 城市列表Bean {
+      /**
+       * 区域名称 : 武汉市
+       * id_qy_parent : 0
+       * 经度 : 114.299
+       * 纬度 : 30.5844
+       * 级别 : 1
+       * 显示顺序 : 19
+       * id : 420100
+       */
+
+      private String 区域名称;
+      private int id_qy_parent;
+      private double 经度;
+      private double 纬度;
+      private int 级别;
+      private int 显示顺序;
+      private int id;
+
+
+      public String get区域名称() { return 区域名称;}
+
+
+      public void set区域名称(String 区域名称) { this.区域名称 = 区域名称;}
+
+
+      public int getId_qy_parent() { return id_qy_parent;}
+
+
+      public void setId_qy_parent(int id_qy_parent) { this.id_qy_parent = id_qy_parent;}
+
+
+      public double get经度() { return 经度;}
+
+
+      public void set经度(double 经度) { this.经度 = 经度;}
+
+
+      public double get纬度() { return 纬度;}
+
+
+      public void set纬度(double 纬度) { this.纬度 = 纬度;}
+
+
+      public int get级别() { return 级别;}
+
+
+      public void set级别(int 级别) { this.级别 = 级别;}
+
+
+      public int get显示顺序() { return 显示顺序;}
+
+
+      public void set显示顺序(int 显示顺序) { this.显示顺序 = 显示顺序;}
+
+
+      public int getId() { return id;}
+
+
+      public void setId(int id) { this.id = id;}
+    }
+
+    public static class 推荐房源Bean {
+      /**
+       * id_yh : 311
+       * id_yh_wt : 509
+       * id_yh_ys : 809
+       * id_yh_dj : 41
+       * id_yh_zdwt : 585
+       * id_bm : 13
+       * id_qy : 420105005
+       * id_lp : 312
+       * 房源编号 : GAIWOCXGVX
+       * 房源状态 : 已租
+       * 盘 : 店面可见
+       * 委托 : 1
+       * 委托日期 : 2005-07-07T21:28:32
+       * 限时 : 1
+       * 有匙 : 1
+       * 推荐 : 1
+       * 独家 : 1
+       * 装修 : 简单装修
+       * 产权性质 : 原始产权
+       * 产权年限 : 189
+       * 出售 : 0
+       * 出租 : 0
+       * 租赁委托 : 1
+       * 售价 : 9454160.0
+       * 租价 : 7977340.0
+       * 有效联系 : 1
+       * 带看次数 : 26755
+       * 现状 : []
+       * 房源相关文件 : ["http://yssoft-oss.oss-cn-shenzhen.aliyuncs.com/Images/t11.jpg?Expires=1488329397&OSSAccessKeyId=LTAIyA0qfsmsrDLZ&Signature=sZOJGZE4u923gxCOFugI0bbcr%2BM%3D"]
+       * 业主姓名 : 欧吉昂
+       * 业主性别 : 男
+       * 业主电话 : 13257694128
+       * 业主备用电话 : 15547936856
+       * 两证情况 : 2年以内
+       * 电梯 : 1
+       * 特色标签 : []
+       * 房源备注 : EKSLGYZBNRDHXXPIUCISVXNZCDVIRJIROFIKMXAFVVMMEOROUPCZHCEWYXTQLXCSNYTLCEAIWXUVGPEXBUCIUGT
+       * 房源审核状态 : 已审核
+       * 房源待审核操作 : []
+       * 房源全员最后跟进时间 : 2017-02-25T00:58:51
+       * 房源委托人最后跟进时间 : 2017-02-27T01:02:47
+       * 创建时间 : 2005-07-07T21:28:32
+       * 更新时间 : 2017-02-27T17:59:01
+       * 座栋号 : I
+       * 单元号 : 2
+       * 楼型 :
+       * 房号 : 2119
+       * 总楼层 : 227
+       * 楼层 : 217
+       * 楼层区间 : 中层
+       * 室数 : 4
+       * 厅数 : 2
+       * 阳台 : 0
+       * 卫数 : 5
+       * 厨房 : 2
+       * 朝向 : 阳光
+       * 建筑面积 : 8387950.0
+       * 房屋用途 : 厂房
+       * 建筑类型 : 钢结构
+       * 年代 : 2005
+       * id : 3
+       */
+
+      private int id_yh;
+      private int id_yh_wt;
+      private int id_yh_ys;
+      private int id_yh_dj;
+      private int id_yh_zdwt;
+      private int id_bm;
+      private int id_qy;
+      private int id_lp;
+      private String 房源编号;
+      private String 房源状态;
+      private String 盘;
+      private int 委托;
+      private String 委托日期;
+      private int 限时;
+      private int 有匙;
+      private int 推荐;
+      private int 独家;
+      private String 装修;
+      private String 产权性质;
+      private int 产权年限;
+      private int 出售;
+      private int 出租;
+      private int 租赁委托;
+      private double 售价;
+      private double 租价;
+      private int 有效联系;
+      private int 带看次数;
+      private String 业主姓名;
+      private String 业主性别;
+      private String 业主电话;
+      private String 业主备用电话;
+      private String 两证情况;
+      private int 电梯;
+      private String 房源备注;
+      private String 房源审核状态;
+      private String 房源全员最后跟进时间;
+      private String 房源委托人最后跟进时间;
+      private String 创建时间;
+      private String 更新时间;
+      private String 座栋号;
+      private int 单元号;
+      private String 楼型;
+      private int 房号;
+      private int 总楼层;
+      private int 楼层;
+      private String 楼层区间;
+      private int 室数;
+      private int 厅数;
+      private int 阳台;
+      private int 卫数;
+      private int 厨房;
+      private String 朝向;
+      private double 建筑面积;
+      private String 房屋用途;
+      private String 建筑类型;
+      private int 年代;
+      private int id;
+      private List<?> 现状;
+      private List<String> 房源相关文件;
+      private List<?> 特色标签;
+      private List<?> 房源待审核操作;
+
+
+      public int getId_yh() { return id_yh;}
+
+
+      public void setId_yh(int id_yh) { this.id_yh = id_yh;}
+
+
+      public int getId_yh_wt() { return id_yh_wt;}
+
+
+      public void setId_yh_wt(int id_yh_wt) { this.id_yh_wt = id_yh_wt;}
+
+
+      public int getId_yh_ys() { return id_yh_ys;}
+
+
+      public void setId_yh_ys(int id_yh_ys) { this.id_yh_ys = id_yh_ys;}
+
+
+      public int getId_yh_dj() { return id_yh_dj;}
+
+
+      public void setId_yh_dj(int id_yh_dj) { this.id_yh_dj = id_yh_dj;}
+
+
+      public int getId_yh_zdwt() { return id_yh_zdwt;}
+
+
+      public void setId_yh_zdwt(int id_yh_zdwt) { this.id_yh_zdwt = id_yh_zdwt;}
+
+
+      public int getId_bm() { return id_bm;}
+
+
+      public void setId_bm(int id_bm) { this.id_bm = id_bm;}
+
+
+      public int getId_qy() { return id_qy;}
+
+
+      public void setId_qy(int id_qy) { this.id_qy = id_qy;}
+
+
+      public int getId_lp() { return id_lp;}
+
+
+      public void setId_lp(int id_lp) { this.id_lp = id_lp;}
+
+
+      public String get房源编号() { return 房源编号;}
+
+
+      public void set房源编号(String 房源编号) { this.房源编号 = 房源编号;}
+
+
+      public String get房源状态() { return 房源状态;}
+
+
+      public void set房源状态(String 房源状态) { this.房源状态 = 房源状态;}
+
+
+      public String get盘() { return 盘;}
+
+
+      public void set盘(String 盘) { this.盘 = 盘;}
+
+
+      public int get委托() { return 委托;}
+
+
+      public void set委托(int 委托) { this.委托 = 委托;}
+
+
+      public String get委托日期() { return 委托日期;}
+
+
+      public void set委托日期(String 委托日期) { this.委托日期 = 委托日期;}
+
+
+      public int get限时() { return 限时;}
+
+
+      public void set限时(int 限时) { this.限时 = 限时;}
+
+
+      public int get有匙() { return 有匙;}
+
+
+      public void set有匙(int 有匙) { this.有匙 = 有匙;}
+
+
+      public int get推荐() { return 推荐;}
+
+
+      public void set推荐(int 推荐) { this.推荐 = 推荐;}
+
+
+      public int get独家() { return 独家;}
+
+
+      public void set独家(int 独家) { this.独家 = 独家;}
+
+
+      public String get装修() { return 装修;}
+
+
+      public void set装修(String 装修) { this.装修 = 装修;}
+
+
+      public String get产权性质() { return 产权性质;}
+
+
+      public void set产权性质(String 产权性质) { this.产权性质 = 产权性质;}
+
+
+      public int get产权年限() { return 产权年限;}
+
+
+      public void set产权年限(int 产权年限) { this.产权年限 = 产权年限;}
+
+
+      public int get出售() { return 出售;}
+
+
+      public void set出售(int 出售) { this.出售 = 出售;}
+
+
+      public int get出租() { return 出租;}
+
+
+      public void set出租(int 出租) { this.出租 = 出租;}
+
+
+      public int get租赁委托() { return 租赁委托;}
+
+
+      public void set租赁委托(int 租赁委托) { this.租赁委托 = 租赁委托;}
+
+
+      public double get售价() { return 售价;}
+
+
+      public void set售价(double 售价) { this.售价 = 售价;}
+
+
+      public double get租价() { return 租价;}
+
+
+      public void set租价(double 租价) { this.租价 = 租价;}
+
+
+      public int get有效联系() { return 有效联系;}
+
+
+      public void set有效联系(int 有效联系) { this.有效联系 = 有效联系;}
+
+
+      public int get带看次数() { return 带看次数;}
+
+
+      public void set带看次数(int 带看次数) { this.带看次数 = 带看次数;}
+
+
+      public String get业主姓名() { return 业主姓名;}
+
+
+      public void set业主姓名(String 业主姓名) { this.业主姓名 = 业主姓名;}
+
+
+      public String get业主性别() { return 业主性别;}
+
+
+      public void set业主性别(String 业主性别) { this.业主性别 = 业主性别;}
+
+
+      public String get业主电话() { return 业主电话;}
+
+
+      public void set业主电话(String 业主电话) { this.业主电话 = 业主电话;}
+
+
+      public String get业主备用电话() { return 业主备用电话;}
+
+
+      public void set业主备用电话(String 业主备用电话) { this.业主备用电话 = 业主备用电话;}
+
+
+      public String get两证情况() { return 两证情况;}
+
+
+      public void set两证情况(String 两证情况) { this.两证情况 = 两证情况;}
+
+
+      public int get电梯() { return 电梯;}
+
+
+      public void set电梯(int 电梯) { this.电梯 = 电梯;}
+
+
+      public String get房源备注() { return 房源备注;}
+
+
+      public void set房源备注(String 房源备注) { this.房源备注 = 房源备注;}
+
+
+      public String get房源审核状态() { return 房源审核状态;}
+
+
+      public void set房源审核状态(String 房源审核状态) { this.房源审核状态 = 房源审核状态;}
+
+
+      public String get房源全员最后跟进时间() { return 房源全员最后跟进时间;}
+
+
+      public void set房源全员最后跟进时间(String 房源全员最后跟进时间) { this.房源全员最后跟进时间 = 房源全员最后跟进时间;}
+
+
+      public String get房源委托人最后跟进时间() { return 房源委托人最后跟进时间;}
+
+
+      public void set房源委托人最后跟进时间(String 房源委托人最后跟进时间) { this.房源委托人最后跟进时间 = 房源委托人最后跟进时间;}
+
+
+      public String get创建时间() { return 创建时间;}
+
+
+      public void set创建时间(String 创建时间) { this.创建时间 = 创建时间;}
+
+
+      public String get更新时间() { return 更新时间;}
+
+
+      public void set更新时间(String 更新时间) { this.更新时间 = 更新时间;}
+
+
+      public String get座栋号() { return 座栋号;}
+
+
+      public void set座栋号(String 座栋号) { this.座栋号 = 座栋号;}
+
+
+      public int get单元号() { return 单元号;}
+
+
+      public void set单元号(int 单元号) { this.单元号 = 单元号;}
+
+
+      public String get楼型() { return 楼型;}
+
+
+      public void set楼型(String 楼型) { this.楼型 = 楼型;}
+
+
+      public int get房号() { return 房号;}
+
+
+      public void set房号(int 房号) { this.房号 = 房号;}
+
+
+      public int get总楼层() { return 总楼层;}
+
+
+      public void set总楼层(int 总楼层) { this.总楼层 = 总楼层;}
+
+
+      public int get楼层() { return 楼层;}
+
+
+      public void set楼层(int 楼层) { this.楼层 = 楼层;}
+
+
+      public String get楼层区间() { return 楼层区间;}
+
+
+      public void set楼层区间(String 楼层区间) { this.楼层区间 = 楼层区间;}
+
+
+      public int get室数() { return 室数;}
+
+
+      public void set室数(int 室数) { this.室数 = 室数;}
+
+
+      public int get厅数() { return 厅数;}
+
+
+      public void set厅数(int 厅数) { this.厅数 = 厅数;}
+
+
+      public int get阳台() { return 阳台;}
+
+
+      public void set阳台(int 阳台) { this.阳台 = 阳台;}
+
+
+      public int get卫数() { return 卫数;}
+
+
+      public void set卫数(int 卫数) { this.卫数 = 卫数;}
+
+
+      public int get厨房() { return 厨房;}
+
+
+      public void set厨房(int 厨房) { this.厨房 = 厨房;}
+
+
+      public String get朝向() { return 朝向;}
+
+
+      public void set朝向(String 朝向) { this.朝向 = 朝向;}
+
+
+      public double get建筑面积() { return 建筑面积;}
+
+
+      public void set建筑面积(double 建筑面积) { this.建筑面积 = 建筑面积;}
+
+
+      public String get房屋用途() { return 房屋用途;}
+
+
+      public void set房屋用途(String 房屋用途) { this.房屋用途 = 房屋用途;}
+
+
+      public String get建筑类型() { return 建筑类型;}
+
+
+      public void set建筑类型(String 建筑类型) { this.建筑类型 = 建筑类型;}
+
+
+      public int get年代() { return 年代;}
+
+
+      public void set年代(int 年代) { this.年代 = 年代;}
+
+
+      public int getId() { return id;}
+
+
+      public void setId(int id) { this.id = id;}
+
+
+      public List<?> get现状() { return 现状;}
+
+
+      public void set现状(List<?> 现状) { this.现状 = 现状;}
+
+
+      public List<String> get房源相关文件() { return 房源相关文件;}
+
+
+      public void set房源相关文件(List<String> 房源相关文件) { this.房源相关文件 = 房源相关文件;}
+
+
+      public List<?> get特色标签() { return 特色标签;}
+
+
+      public void set特色标签(List<?> 特色标签) { this.特色标签 = 特色标签;}
+
+
+      public List<?> get房源待审核操作() { return 房源待审核操作;}
+
+
+      public void set房源待审核操作(List<?> 房源待审核操作) { this.房源待审核操作 = 房源待审核操作;}
+    }
+  }
+}
